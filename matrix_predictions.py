8466f757ff68c4a01620b941cf967a8b28ebeb23
Eitan <erapapor@caltech.edu>
2022-10-21 17:54:31 -0700

    distinguishability stuff done

--- matrix_predictions.py
+++ matrix_predictions.py
@@ -1,28 +1,40 @@
-from numpy import np
+import numpy as np
 from sklearn.metrics import mean_squared_error
+import concepts as concepts
 
 # Explore large 2x2 matrix subset and calculate predictions from each stationary
 # concept. 
 
-# For now, we will check only zero-sum games. Need to think of a good way to
-# exhaustively check. Perhaps we can set a maximum payoff and just use a brute
-# force approach. 
+#Check for negatives and fractions
+def check_conditions(game):
+    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = concepts.transform_coords(game)
+    if any(n < 0 for n in (a_l, a_r, b_u, b_d)):
+       #Base payoffs must be non-negative
+       return False
+    if any(n <= 0 for n in (c_l, c_r, d_u, d_d)):
+       #Payoff differences must be positive
+       return False
+    return True
 
-def generate_zero_sum_games(min_payoff, max_payoff):
+def generate_games(number):
     '''
-    Generating zero-sum games limits the number of free parameters to four,
-    instead of eight. 
-
-    Generate a list of all zero-sum games limited by the given max_payoff and
-    min_payoff. Games are represented as an eight-tuple. 
+    Generate a list of all games up to some number. Games are represented as an eight-tuple. 
     '''
     games = []
 
-    for i in range(min_payoff, max_payoff + 1):
-        for j in range(min_payoff, max_payoff + 1):
-            for k in range(min_payoff, max_payoff + 1):
-                for l in range(min_payoff, max_payoff + 1):
-                    games.append((i, 0 - i, j, 0 - j, k, 0 - k, l, 0 - l))
+    for i in range(number+1):
+        for j in range(number+1):
+            for k in range(number+1):
+                for l in range(number+1):
+                    for v in range(number+1):
+                        for b in range(number+1):
+                            for n in range(number+1):
+                                for m in range(number+1):
+                                    game = (i,j,k,l,v,b,n,m)
+                                    if(check_conditions(game) == False):
+                                        continue
+                                    else:
+                                        games.append(game)
 
     return games
 
@@ -44,14 +56,29 @@ def get_distance(pdxns_1, pdxns_2):
     return np.array(dists)
 
 
-def main(min_payoff, max_payoff):
+def main(number):
     
-    games = generate_zero_sum_games(min_payoff, max_payoff)
+    games = generate_games(number)
 
-    ibe_pdxns = [impulse_balance_equilibrium(game) for game in games]
-    ne_pdxns = [nash_equilibrium(game) for game in games]
+    #Get predictions for suitable games
+    ibe_pdxns = [concepts.impulse_balance_equilibrium(game) for game in games]
+    ne_pdxns = [concepts.nash_equilibrium(game) for game in games]
+    qre_pdxns = [concepts.quantal_response_equilibrium(game) for game in games]
+    
+    distinguishability_values = []
+    for i in range(len(games)):
+        #Get distinguishability for each pdxn compared to each other
+        distance_1 = (get_distance(ne_pdxns[i],qre_pdxns[i])[0],get_distance(ne_pdxns[i],qre_pdxns[i])[2])
+        #distance_2 = (get_distance(ne_pdxns[i],ibe_pdxns[i])[0],get_distance(ne_pdxns[i],ibe_pdxns[i])[2])
+        #distance_3 = (get_distance(ibe_pdxns[i],qre_pdxns[i])[0],get_distance(ibe_pdxns[i],qre_pdxns[i])[2])
+        
+        #Combine each difference
+        total_distance_1 = np.sqrt(distance_1[0]**2 + distance_1[1]**2)
+        #total_distance_2 = np.sqrt(distance_2[0]**2 + distance_2[1]**2)
+        #total_distance_3 = np.sqrt(distance_3[0]**2 + distance_3[1]**2)
+        
+        #Combine all totals
+        distinguishability = np.sqrt(total_distance_1**2) #+ total_distance_2**2 + total_distance_3**2)
+        distinguishability_values.append(distinguishability)
 
-    # What should our strategy be for finding matrices which are very different
-    # across all games? Some kind of exhaustive search? Maybe sort the games
-    # from highest-lowest difference, and find the first one present in all the
-    # lists?
+    return distinguishability_values, games, (ibe_pdxns,ne_pdxns,qre_pdxns)
