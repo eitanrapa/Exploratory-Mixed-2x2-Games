0f11f1bf0be14099d1231a794dd8fe0532b86bce
pipparichter <philippa.a.richter@gmail.com>
2022-10-23 22:09:23 -0700

    some stuffs

--- concepts.py
+++ concepts.py
@@ -1,38 +1,105 @@
 import numpy as np
 from scipy.optimize import fsolve, least_squares
 
-#U is up, D is down, L is left, R is right
-#Player 1 is the row player, having up and down choices
-#Player 2 is the col player, having left and right choices
-#c_l and c_r are Player 1's payoff differences
-#d_u and d_d are Player 2's payoff differences
-#p_u and p_d are Player 1's choice probability for Up and Down
-#q_l and q_r are Player 2's choice probability for Left and Right
+# U is up, D is down, L is left, R is right
+# Player 1 is the row player, having up and down choices
+# Player 2 is the col player, having left and right choices
+# c_l and c_r are Player 1's payoff differences
+# d_u and d_d are Player 2's payoff differences
+# p_u and p_d are Player 1's choice probability for Up and Down
+# q_l and q_r are Player 2's choice probability for Left and Right
 
-def transform_coords(game):
-    top_left_left, top_left_right, top_right_left, top_right_right, bot_left_left, bot_left_right, bot_right_left, bot_right_right = game
-    a_r = top_right_left
-    b_u = top_left_right
-    a_l = bot_left_left
-    b_d = bot_right_right
-    c_l = top_left_left - a_l
-    d_u = top_right_right - b_u
-    d_d = bot_left_right - b_d
-    c_r = bot_right_left - a_r
+def game_to_matrix(game):
+    '''
+    Convert values of a game back into a matrix.
+    Matrix values are given in order LU_1, LU_2, RU_1, RU_2, 
+    LD_1, LD_2, RD_1, RD_2.
+    '''
+    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = game
+    
+    RU_1, LU_2, LD_1, RD_2 = a_r, b_u, a_l, b_d
+    LU_1 = a_l + c_l
+    RU_2 = b_u + d_u
+    LD_2 = b_d + d_d
+    RD_1 = a_r + c_r
+
+    return LU_1, LU_2, RU_1, RU_2, LD_1, LD_2, RD_1, RD_2
+
+
+def matrix_to_game(matrix):
+    '''
+    Convert values of a matrix to values which are understood by the different
+    models (a_l, a_r, etc.).
+
+    a_l, a_r, b_u, and b_d need to be greater than or equal to zero.
+    c_l, c_r, d_u, d_d need to be greater than zero. 
+    '''
+    LU_1, LU_2, RU_1, RU_2, LD_1, LD_2, RD_1, RD_2 = matrix
+    
+    a_r, b_u, a_l, b_d = RU_1, LU_2, LD_1, RD_2
+    c_l = LU_1 - a_l
+    d_u = RU_2 - b_u
+    d_d = LD_2 - b_d
+    c_r = RD_1 - a_r
     return a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d 
 
-def display_matrix(game):
-    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = transform_coords(game)
+def display_matrix(matrix, cell_width=8):
+    '''
+    Print out a nice lil visual of the game in matrix form. 
+    '''
+    LU_1, LU_2, RU_1, RU_2, LD_1, LD_2, RD_1, RD_2 = matrix
+    
+    def spacer(n): # Adjust spacer size based on string length. 
+        return ' ' * (cell_width - len(str(n)))
+    
+    row_1 = f'| {LU_1} ' + spacer(LU_1) + f'| {RU_1} ' + spacer(RU_1) + '|'
+    row_2 = f'|' + spacer(LU_2) + f' {LU_2} |' +  spacer(RU_2)  + f' {RU_2} |'
+    row_3 = f'| {LD_1} ' + spacer(LD_1) + f'| {RD_1} ' + spacer(RD_1) + '|'
+    row_4 = f'|' + spacer(LD_2) + f' {LD_2} |' +  spacer(RD_2)  + f' {RD_2} |'
+    
+    divider ='-' * len(row_1)
+    
+    print(divider)
+    print(row_1)
+    print(row_2)
+    print(divider)
+    print(row_3)
+    print(row_4)
+    print(divider)
 
-    top_left = a_l + c_l
-    top_right = b_u + d_u
-    bottom_left = b_d + d_d
-    bottom_right = a_r + c_r
-    print("(" + str(top_left) + "," + str(b_u) + ")     (" + str(a_r) + "," + str(top_right) + ")\n")
-    print("(" + str(a_l) + "," + str(bottom_left) + ")     (" + str(bottom_right) + "," + str(b_d) + ")")
+def display_game(game, cell_width=8):
+    '''
+    Print out a nice lil visual of the game in the more complicated form.
+    '''
+    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = game # transform_coords(game)
+    
+    def spacer(x=None, y=None): # Adjust spacer size based on string length. 
+        if y is not None: # Accomodate the plus sign. 
+            return ' ' * (cell_width - len(str(x)) - len(str(y)) - 3)
+        else:
+            return ' ' * (cell_width - len(str(x)))
+    
+    row_1 = f'| {a_l} + {c_l} ' + spacer(x=a_l, y=c_l) + f'| {a_r} ' + spacer(x=a_r) + '|'
+    row_2 = f'|' + spacer(x=b_u) + f' {b_u} |' +  spacer(x=b_u, y=d_u)  + f' {b_u} + {d_u} |'
+    row_3 = f'| {a_l} ' + spacer(x=a_l) + f'| {a_r} + {c_r} ' + spacer(x=a_r, y=c_r) + '|'
+    row_4 = f'|' + spacer(x=b_d, y=d_d) + f' {b_d} + {d_d} |' +  spacer(x=b_d) + f' {b_d} |'
+    
+    divider ='-' * len(row_1)
+    
+    print(divider)
+    print(row_1)
+    print(row_2)
+    print(divider)
+    print(row_3)
+    print(row_4)
+    print(divider)
+
 
 def nash_equilibrium(game):
-    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = transform_coords(game)
+    '''
+    Just do the thing for a simple Nash equilibrium. 
+    '''
+    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = game
     p_u = d_d / (d_u + d_d)
     p_d = d_u / (d_u + d_d)
     q_l = c_r / (c_l + c_r)
@@ -46,14 +113,16 @@ def transform(s_i, payoff):
     '''
     Transforms a payoff based on the security level. 
     '''
-    return min(0, payoff - (payoff - s_i)/2)
+    if payoff > s_i:
+        payoff = payoff - (payoff - s_i)/2
+    return payoff
 
 def impulse_balance_equilibrium(game):
     '''
     Returns the predicted probability of each strategy choice according to the
     Impulse Balance Equilibrium model. 
     '''
-    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = transform_coords(game)
+    a_l, a_r, b_u, b_d, c_l, c_r, d_u, d_d = game # transform_coords(game)
     
     # Security levels for players 1 and 2.
     s_1 = max(min(a_l + c_l, a_r), min(a_l, a_r + c_r))
@@ -75,10 +144,6 @@ def impulse_balance_equilibrium(game):
     c_r_star = max(RU_1 - s_1, RD_1 - s_1)
     d_u_star = max(LU_2 - s_2, RU_2 - s_2)
     d_d_star = max(LD_2 - s_2, RD_2 - s_2)
-
-    #temp fix, not sure what is going on
-    if(c_r_star==0 or d_d_star==0):
-        return None
     
     c = c_l_star / c_r_star
     d = d_u_star / d_d_star
